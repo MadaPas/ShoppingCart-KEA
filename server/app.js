a0fe890e6d5ffc04c433ba656758504d11116c6b
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-04-19 16:02:32 +0200

    refact

--- server/app.js
+++ server/app.js
@@ -1,17 +1,19 @@
 /* eslint-disable no-console */
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { connectDB } = require('./db/index');
 
-require('dotenv').config();
-
 const routes = require('./routes/index');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false,
+}));
 app.use(bodyParser.json());
+
 app.use(cors());
 
 app.get('/', (req, res) => {
