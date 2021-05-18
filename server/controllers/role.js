fbb77409353ef89b8274ba36cab29e86498e7d9e
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-05-18 19:50:33 +0200

    roles -- crud refactoring done

--- server/controllers/role.js
+++ server/controllers/role.js
@@ -7,100 +7,128 @@ const {
 } = require('../models/Role');
 
 const getAllRoles = async (req, res) => {
-  await Role.find({}, (err, roles) => {
-    if (err) res.status(500).send(err);
-    res.status(200).json(roles);
-  });
+  try {
+    Role.find({})
+      .then(roles => res.status(200).json({
+        message: 'Data retrieved successfully.',
+        data: roles,
+      }))
+      .catch(err => res.json({
+        error: err,
+      }));
+  } catch (err) {
+    return res.status(500).json({
+      message: `Internal server error: ${err}`,
+    });
+  }
 };
 
-const getRole = (req, res) => {
-  if (!req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
-    return res.status(404).json('Wrong Role id format. Try again.');
+const getRole = (req, res) => Role.findById(req.params.id, (err, role) => {
+  if (role === null || role.length === 0) {
+    return res.status(404).json({
+      message: 'No role found. Please try again.',
+    });
   }
-  return Role.findById(req.params.id, (err, role) => {
-    if (role === null || role.length === 0) {
-      return res.status(404).json('No role found');
-    }
-    if (err) {
-      return res.status(500).send(err);
-    }
-    return res.status(200).json(role);
+  if (err) {
+    return res.status(500).json({
+      message: `Internal server error: ${err}`,
+    });
+  }
+  return res.status(200).json({
+    message: 'Data retrieved successfully.',
+    data: role,
   });
-};
+});
 
 const addRole = asyncHandler(async (req, res) => {
   const {
     name,
   } = req.body;
-  const prooductExists = await Role.findOne({ name });
+  const exists = await Role.findOne({ name });
 
-  if (prooductExists) {
-    return res.status(400).json('This role already exists.');
+  if (exists) {
+    return res.status(400).json({
+      message: 'This role already exists.',
+    });
   }
 
-  const role = await Role.create({
-    name,
-  });
+  const role = await Role.create(req.body);
 
-  return res.status(201).json({
-    name: role.name,
+  return res.status(200).json({
+    message: 'Role created successfully.',
+    data: role,
   });
 });
 
 const updateRole = async (req, res) => {
   try {
-    if (!req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
-      return res.status(404).json('Wrong role id format. Try again.');
-    }
-    const newRole = await Role.findById(req.params.id, (err, role) => {
+    await Role.findById(req.params.id, (err, role) => {
       if (role === null || role.length === 0) {
-        return res.status(404).json('No role found');
+        return res.status(404).json({
+          message: 'No role found. Please try again.',
+        });
       }
       if (err) {
-        return res.status(500).send(err);
+        return res.status(500).json({
+          message: `Internal server error: ${err}`,
+        });
       }
       Role.findByIdAndUpdate(
         req.params.id,
         req.body,
         { new: true },
         (error, pr) => {
-          if (error) return res.status(500).send(error);
-          return res.send(pr);
+          if (error) {
+            return res.status(500).json({
+              message: `Internal server error: ${error}`,
+            });
+          }
+          return res.status(200).json({
+            message: 'Role updated successfully.',
+            data: pr,
+          });
         },
       );
     });
-    return newRole;
   } catch (err) {
-    res.status(500).json('Internal server error');
+    return res.status(500).json({
+      message: `Internal server error: ${err}`,
+    });
   }
 };
 
 const deleteRole = async (req, res) => {
   try {
-    if (!req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
-      return res.status(404).json('Wrong role id format. Try again.');
-    }
     await Role.findById(req.params.id, (err, role) => {
       if (role === null || role.length === 0) {
-        return res.status(404).json('No role found');
+        return res.status(404).json({
+          message: 'No role found. Please try again.',
+        });
       }
       if (err) {
-        return res.status(500).send(err);
+        return res.status(500).json({
+          message: `Internal server error: ${err}`,
+        });
       }
       Role.findByIdAndRemove(
         req.params.id,
         (error, pr) => {
-          if (error) return res.status(500).send(error);
-          const response = {
-            message: 'Role successfully deleted',
+          if (error) {
+            return res.status(500).json({
+              message: `Internal server error: ${error}`,
+            });
+          }
+          return res.status(200).json({
+            message: 'Role deleted successfully.',
             id: pr._id,
-          };
-          return res.status(200).send(response);
+          });
         },
       );
     });
   } catch (err) {
-    res.status(500).json('Internal server error');
+    return res.status(500).json({
+      message: `Internal server error: ${err}`,
+    });
   }
 };
 
