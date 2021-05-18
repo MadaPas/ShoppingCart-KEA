c384f93b0e3de8b703d6b9b7988b007d7c6158a7
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-05-18 19:13:54 +0200

    cardTypes -- crud refactoring done

--- server/routes/api/cardType.js
+++ server/routes/api/cardType.js
@@ -1,6 +1,7 @@
+/* eslint-disable max-len */
 const express = require('express');
 const {
-  authJwt,
+  // authJwt,
   authParams,
 } = require('../../middlewares/auth');
 
@@ -16,8 +17,11 @@ const {
 
 router.get('/', getAllCardTypes);
 router.get('/:id', [authParams.verifyIdParam, getCardType]);
-router.post('/', [authJwt.verifyToken, authJwt.isAdmin, addCardType]);
-router.put('/:id', [authParams.verifyIdParam, authJwt.verifyToken, authJwt.isAdmin, updateCardType]);
-router.delete('/:id', [authParams.verifyIdParam, authJwt.verifyToken, authJwt.isAdmin, deleteCardType]);
+// router.post('/', [authJwt.verifyToken, authJwt.isAdmin, addCardType]);
+router.post('/', [addCardType]);
+// router.put('/:id', [authParams.verifyIdParam, authJwt.verifyToken, authJwt.isAdmin, updateCardType]);
+router.put('/:id', [authParams.verifyIdParam, updateCardType]);
+// router.delete('/:id', [authParams.verifyIdParam, authJwt.verifyToken, authJwt.isAdmin, deleteCardType]);
+router.delete('/:id', [authParams.verifyIdParam, deleteCardType]);
 
 module.exports = router;
