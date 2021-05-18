9846a13f7177e4208ecb59cb61da7cab07264cb1
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-05-18 19:25:05 +0200

    products -- crud refactoring done

--- server/routes/api/product.js
+++ server/routes/api/product.js
@@ -1,6 +1,7 @@
+/* eslint-disable max-len */
 const express = require('express');
 const {
-  authJwt,
+  // authJwt,
   authParams,
 } = require('../../middlewares/auth');
 
@@ -14,10 +15,16 @@ const {
   deleteProduct,
 } = require('../../controllers/product');
 
+// router.get('/', getAllProducts);
+// router.get('/:id', [authParams.verifyIdParam, getProduct]);
+// router.post('/', [authJwt.verifyToken, authJwt.isEmployeeOrAdmin, addProduct]);
+// router.put('/:id', [authParams.verifyIdParam, authJwt.verifyToken, authJwt.isEmployeeOrAdmin, updateProduct]);
+// router.delete('/:id', [authParams.verifyIdParam, authJwt.verifyToken, authJwt.isAdmin, deleteProduct]);
+
 router.get('/', getAllProducts);
 router.get('/:id', [authParams.verifyIdParam, getProduct]);
-router.post('/', [authJwt.verifyToken, authJwt.isEmployeeOrAdmin, addProduct]);
-router.put('/:id', [authParams.verifyIdParam, authJwt.verifyToken, authJwt.isEmployeeOrAdmin, updateProduct]);
-router.delete('/:id', [authParams.verifyIdParam, authJwt.verifyToken, authJwt.isAdmin, deleteProduct]);
+router.post('/', [addProduct]);
+router.put('/:id', [authParams.verifyIdParam, updateProduct]);
+router.delete('/:id', [authParams.verifyIdParam, deleteProduct]);
 
 module.exports = router;
