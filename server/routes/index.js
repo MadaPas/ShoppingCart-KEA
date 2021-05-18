54148dfd67fe819c7fd0393352ae3c81abb1da4d
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-05-18 17:19:25 +0200

    favorite products -- auth priviledges

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,19 +5,19 @@ const router = express.Router();
 const brandsRoutes = require('./api/brand.js');
 const cardTypesRoutes = require('./api/cardType.js');
 const cartItemsRoutes = require('./api/cartItem.js');
+const favouriteProductsRoutes = require('./api/favouriteProduct.js');
 
 const userRoutes = require('./api/user.js');
 const productRoutes = require('./api/product.js');
 const rolesRoutes = require('./api/role.js');
-const favouritesRoutes = require('./api/favourite.js');
 
 router.use('/brands', brandsRoutes);
 router.use('/cards', cardTypesRoutes);
 router.use('/cartItems', cartItemsRoutes);
+router.use('/favourites', favouriteProductsRoutes);
 
 router.use('/users', userRoutes);
 router.use('/products', productRoutes);
 router.use('/roles', rolesRoutes);
-router.use('/favourites', favouritesRoutes);
 
 module.exports = router;
