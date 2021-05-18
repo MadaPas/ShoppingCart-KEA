afb7e3cc4fad1cf0f87fcf1b8ec8ef01ecc3c219
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-05-18 19:14:26 +0200

    cart items -- crud refactoring done

--- server/controllers/cartItem.js
+++ server/controllers/cartItem.js
@@ -1,14 +1,23 @@
+/* eslint-disable consistent-return */
 const {
   CartItem,
 } = require('../models/CartItem');
 
 const getAllCartItems = async (req, res) => {
-  await CartItem.find({}, (err, cartItems) => {
-    if (err) res.status(500).send(err);
-    res.status(200).json(cartItems);
-  });
+  try {
+    CartItem.find({})
+      .then(cartItems => res.status(200).json({
+        message: 'Data retrieved successfully.',
+        data: cartItems,
+      }))
+      .catch(err => res.json({
+        error: err,
+      }));
+  } catch (err) {
+    return res.status(500).json({
+      message: `Internal server error: ${err}`,
+    });
+  }
 };
-
-// add cart item handled in controllers/invoice.js in the transaction in the object addInvoice
 
 module.exports.getAllCartItems = getAllCartItems;
