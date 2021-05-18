4f52ad9d60d45c3accaa279b296c6ef5f1691eca
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-05-18 20:10:46 +0200

    users -- crud refactoring done

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -6,99 +6,162 @@ const asyncHandler = require('express-async-handler');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
-const { User } = require('../models/User');
+const {
+  User,
+} = require('../models/User');
 
 const getAllUsers = async (req, res) => {
-  await User.find({}, (err, users) => {
-    if (err) res.status(500).send(err);
-    res.status(200).json(users);
-  });
+  try {
+    await User.find({})
+      .then(users => res.status(200).json({
+        message: 'Data retrieved successfully.',
+        data: users,
+      }))
+      .catch(err => res.json({
+        error: err,
+      }));
+  } catch (err) {
+    return res.status(500).json({
+      message: `Internal server error: ${err}`,
+    });
+  }
 };
 
-const getUser = (req, res) => {
-  if (!req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
-    return res.status(404).json('No user found');
+const getUser = (req, res) => User.findById(req.params.id, (err, user) => {
+  if (user === null || user.length === 0) {
+    return res.status(404).json({
+      message: 'No user found. Please try again.',
+    });
   }
-  return User.findById(req.params.id, (err, user) => {
-    if (user.length === 0) {
-      return res.status(404).json('No user found');
-    }
-    if (err) {
-      return res.status(500).send(err);
-    }
-    return res.status(200).json(user);
+  if (err) {
+    return res.status(500).json({
+      message: `Internal server error: ${err}`,
+    });
+  }
+  return res.status(200).json({
+    message: 'Data retrieved successfully.',
+    data: user,
   });
-};
+});
 
 const deleteUser = async (req, res) => {
   try {
     await User.findById(req.params.id, (err, user) => {
       if (user === null || user.length === 0) {
-        return res.status(404).json('No user found');
+        return res.status(404).json({
+          message: 'No user found. Please try again.',
+        });
       }
       if (err) {
-        return res.status(500).send(err);
+        return res.status(500).json({
+          message: `Internal server error: ${err}`,
+        });
       }
       User.findByIdAndRemove(
         req.params.id,
         (error, pr) => {
-          if (error) return res.status(500).send(error);
-          const response = {
-            message: 'User successfully deleted',
+          if (error) {
+            return res.status(500).json({
+              message: `Internal server error: ${error}`,
+            });
+          }
+          return res.status(200).json({
+            message: 'User deleted successfully.',
             id: pr._id,
-          };
-          return res.status(200).send(response);
+          });
         },
       );
     });
   } catch (err) {
-    return res.status(500).json('Internal server error');
+    return res.status(500).json({
+      message: `Internal server error: ${err}`,
+    });
   }
 };
 
 const signUpUser = asyncHandler(async (req, res) => {
+  const {
+    role_id,
+    first_name,
+    last_name,
+    email,
+    password,
+  } = req.body;
   try {
+    const exists = await User.findOne({
+      email,
+    });
+
+    if (exists) {
+      return res.status(400).json({
+        message: 'This user already exists.',
+      });
+    }
+
     const newUser = {
-      role_id: req.body.role_id,
-      first_name: req.body.first_name,
-      last_name: req.body.last_name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, process.env.BCRYPT_SALT),
+      role_id,
+      first_name,
+      last_name,
+      email,
+      password: bcrypt.hashSync(password, process.env.BCRYPT_SALT),
     };
-    await User.create({ newUser })
-      .then(data => res.json(data))
-      .catch(err => res.status(404).send(err));
+    await User.create({
+      newUser,
+    })
+      .then(user => res.json({
+        message: 'User created successfully.',
+        data: user,
+      }))
+      .catch(err => res.json({
+        message: `Internal server error: ${err}`,
+      }));
   } catch (err) {
-    return res.status(500).json({ message: 'Internal server error', error: err });
+    return res.status(500).json({
+      message: `Internal server error: ${err}`,
+      error: err,
+    });
   }
 });
 
 const signInUser = asyncHandler(async (req, res) => {
-  const { email } = req.body;
+  const {
+    email,
+    password,
+  } = req.body;
   try {
-    User.findOne({ email })
+    User.findOne({
+      email,
+    })
       .then(data => {
-        if (!bcrypt.compareSync(req.body.password, data.password)) {
-          return res.status(401).send('Unauthorized user, credentials do not match!');
+        if (!bcrypt.compareSync(password, data.password)) {
+          return res.status(401).json({
+            message: 'Unauthorized user, credentials do not match. Try again. ',
+          });
         }
         const token = jwt.sign({
           id: data.id,
         }, process.env.JWT_SECRET, {
           expiresIn: process.env.JWT_EXPIRE,
         });
-        return res.status(200).send({
-          id: data.id,
-          first_name: data.first_name,
-          last_name: data.last_name,
-          email: data.email,
-          role_id: data.role_id,
-          accessToken: token,
+        return res.status(200).json({
+          message: 'Signed in successfully.',
+          data: {
+            id: data.id,
+            first_name: data.first_name,
+            last_name: data.last_name,
+            email: data.email,
+            role_id: data.role_id,
+            accessToken: token,
+          },
         });
       })
-      .catch(err => res.status(404).send(err));
+      .catch(err => res.json({
+        error: err,
+      }));
   } catch (err) {
-    console.log(err);
-    return res.status(500).json('Internal server error');
+    return res.status(500).json({
+      message: `Internal server error: ${err}`,
+    });
   }
 });
 
