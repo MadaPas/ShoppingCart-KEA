5dd53482b2ddfca21cc3dc0f71cbf7904ea781d0
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-05-25 21:32:59 +0200

    swagger -- role

--- server/routes/api/role.js
+++ server/routes/api/role.js
@@ -14,6 +14,138 @@ const {
   deleteRole,
 } = require('../../controllers/role');
 
+/**
+ * @swagger
+ * /api/roles:
+ *   get:
+ *     description: Use to request all roles
+ *     tags:
+ *       - roles
+ *     security:
+ *       -   bearerAuth: []
+ *     responses:
+ *       '200':
+ *         description: A successful response, returned all roles
+ *       '401':
+ *         description: Unauthorized
+ *       '403':
+ *         description: Forbidden, no token provided or require admin role
+ *       '404':
+ *         description: Page not found
+ *       '500':
+ *         description: Internal server error
+ *   post:
+ *     description: Use to add role
+ *     tags:
+ *       - roles
+ *     security:
+ *       -   bearerAuth: []
+ *     parameters:
+ *       - in: body
+ *         name: role
+ *         description: The role to create
+ *         schema:
+ *           type: object
+ *           required:
+ *            - name
+ *           properties:
+ *             name:
+ *               type: string
+ *     responses:
+ *       '201':
+ *         description: Created, added new role
+ *       '401':
+ *         description: Unauthorized
+ *       '403':
+ *         description: Forbidden, no token provided or require admin role
+ *       '404':
+ *         description: Page not found
+ *       '500':
+ *         description: Internal server error
+ * /api/roles/{id}:
+ *   get:
+ *     description: Use to request role
+ *     tags:
+ *       - roles
+ *     security:
+ *       -   bearerAuth: []
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: The ID of the role to return
+ *         schema:
+ *           type: integer
+ *           format: int64
+ *     responses:
+ *       '200':
+ *         description: A successful response, returned role
+ *       '400':
+ *         description: Bad request, wrong id format
+ *       '401':
+ *         description: Unauthorized
+ *       '403':
+ *         description: Forbidden, no token provided or requires admin role
+ *       '404':
+ *         description: Page not found
+ *       '500':
+ *         description: Internal server error
+ *   put:
+ *     description: Use to update role
+ *     tags:
+ *       - roles
+ *     security:
+ *       -   bearerAuth: []
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: The ID of the role to update
+ *         schema:
+ *           type: integer
+ *           format: int64
+ *     responses:
+ *       '200':
+ *         description: A successful response, updated role
+ *       '400':
+ *         description: Bad request, wrong id format
+ *       '401':
+ *         description: Unauthorized
+ *       '403':
+ *         description: Forbidden, no token provided or require admin role
+ *       '404':
+ *         description: Page not found
+ *       '500':
+ *         description: Internal server error
+ *   delete:
+ *     description: Use to delete role
+ *     tags:
+ *       - roles
+ *     security:
+ *       -   bearerAuth: []
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: The ID of the role to delete
+ *         schema:
+ *           type: integer
+ *           format: int64
+ *     responses:
+ *       '204':
+ *         description: No content, deleted role
+ *       '400':
+ *         description: Bad request, wrong id format
+ *       '401':
+ *         description: Unauthorized
+ *       '403':
+ *         description: Forbidden, no token provided or require admin role
+ *       '404':
+ *         description: Page not found
+ *       '500':
+ *         description: Internal server error
+ */
+
 router.get('/', [authJwt.verifyToken, authJwt.isAdmin, getAllRoles]);
 router.get('/:id', [authParams.verifyIdParam, authJwt.verifyToken, authJwt.isAdmin, getRole]);
 router.post('/', [authJwt.isAdmin, authJwt.verifyToken, addRole]);
